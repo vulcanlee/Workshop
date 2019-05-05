605e5a1d03da218a197826ca5c878c4c88e49c97
李進興 Vulcan <vulcan@miniasp.com>
2019-05-05 16:50:10 +0800

    建立 ASP.NET Core Web API 專案

--- LOBCoreLab/Lab01/LOBCore/Program.cs
+++ LOBCoreLab/Lab01/LOBCore/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace LOBApp
+namespace LOBCore
 {
     public class Program
     {
