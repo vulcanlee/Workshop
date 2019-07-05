c3014f52d662cec88c8abe10434f502cfc205663
李進興 Vulcan <vulcan@miniasp.com>
2019-07-05 12:27:01 +0800

    C# 執行緒集區 Thread Pool 對於 ASP.NET Core 專案之同步與非同步設計的影響 延遲時間單位改成毫秒

--- LOBCore/LOBCore/Controllers/RemoteSourceController.cs
+++ LOBCore/LOBCore/Controllers/RemoteSourceController.cs
@@ -84,7 +84,7 @@ namespace LOBCore.Controllers
             ThreadPool.GetAvailableThreads(out workerThreadsAvailable, out completionPortThreadsAvailable);
             ThreadPool.GetMaxThreads(out workerThreadsMax, out completionPortThreadsMax);
             ThreadPool.GetMinThreads(out workerThreadsMin, out completionPortThreadsMin);
-            await Task.Delay(delay * 1000);
+            await Task.Delay(delay);
             var fooUrl = Url.Action("ResponAndAwait2");
             DateTime Complete = DateTime.Now;
             return $"AW:{workerThreadsAvailable} AC:{completionPortThreadsAvailable}" +
@@ -104,7 +104,7 @@ namespace LOBCore.Controllers
             ThreadPool.GetAvailableThreads(out workerThreadsAvailable, out completionPortThreadsAvailable);
             ThreadPool.GetMaxThreads(out workerThreadsMax, out completionPortThreadsMax);
             ThreadPool.GetMinThreads(out workerThreadsMin, out completionPortThreadsMin);
-            Thread.Sleep(delay * 1000);
+            Thread.Sleep(delay);
             var fooUrl = Url.Action("ResponAndAwait2");
             DateTime Complete = DateTime.Now;
             return $"AW:{workerThreadsAvailable} AC:{completionPortThreadsAvailable}" +
