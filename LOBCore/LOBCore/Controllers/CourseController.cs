a7490d6124aa82f874f1abf31794237dde42d4bc
李進興 Vulcan <vulcan@miniasp.com>
2019-12-05 14:33:05 +0800

    將 Lab 用到的 Add API 抽離出來，成為一個 Controller

---
+++ LOBCore/LOBCore/Controllers/CourseController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using LOBCore.DataTransferObject.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LOBCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        [Route("Source3")]
+        [HttpGet]
+        public async Task<string> Source3()
+        {
+            await Task.Delay(2000);
+            return $"Come from Source3 : 23.7 (延遲 3000 ms)";
+        }
+
+        [HttpGet("AddSync/{value1}/{value2}/{delayms}")]
+        public int AddSync(int value1, int value2, int delayms)
+        {
+            Thread.Sleep(delayms);
+            return value1 + value2;
+        }
+        [HttpGet("AddAsync/{value1}/{value2}/{delayms}")]
+        public async Task<int> AddAsync(int value1, int value2, int delayms)
+        {
+            await Task.Delay(delayms);
+            return value1 + value2;
+        }
+        [HttpGet("AddAsyncWithThreadPoolInformation/{value1}/{value2}/{delay}")]
+        public async Task<string> AddAsyncWithThreadPoolInformation(int value1, int value2, int delayms)
+        {
+            DateTime Begin = DateTime.Now;
+            int workerThreadsAvailable;
+            int completionPortThreadsAvailable;
+            int workerThreadsMax;
+            int completionPortThreadsMax;
+            int workerThreadsMin;
+            int completionPortThreadsMin;
+            ThreadPool.GetAvailableThreads(out workerThreadsAvailable, out completionPortThreadsAvailable);
+            ThreadPool.GetMaxThreads(out workerThreadsMax, out completionPortThreadsMax);
+            ThreadPool.GetMinThreads(out workerThreadsMin, out completionPortThreadsMin);
+            await Task.Delay(delayms);
+            var fooUrl = Url.Action("ResponAndAwait2");
+            DateTime Complete = DateTime.Now;
+            return $"AW:{workerThreadsAvailable} AC:{completionPortThreadsAvailable}" +
+                $" MaxW:{workerThreadsMax} MaxC:{completionPortThreadsMax}" +
+                $" MinW:{workerThreadsMin} MinC:{completionPortThreadsMin} ({Begin.TimeOfDay} - {Complete.TimeOfDay})";
+        }
+      
+        [HttpGet("AddSyncWithThreadPoolInformation/{value1}/{value2}/{delay}")]
+        public string AddSyncWithThreadPoolInformation(int value1, int value2, int delayms)
+        {
+            DateTime Begin = DateTime.Now;
+            int workerThreadsAvailable;
+            int completionPortThreadsAvailable;
+            int workerThreadsMax;
+            int completionPortThreadsMax;
+            int workerThreadsMin;
+            int completionPortThreadsMin;
+            ThreadPool.GetAvailableThreads(out workerThreadsAvailable, out completionPortThreadsAvailable);
+            ThreadPool.GetMaxThreads(out workerThreadsMax, out completionPortThreadsMax);
+            ThreadPool.GetMinThreads(out workerThreadsMin, out completionPortThreadsMin);
+            Thread.Sleep(delayms);
+            var fooUrl = Url.Action("ResponAndAwait2");
+            DateTime Complete = DateTime.Now;
+            return $"AW:{workerThreadsAvailable} AC:{completionPortThreadsAvailable}" +
+                $" MaxW:{workerThreadsMax} MaxC:{completionPortThreadsMax}" +
+                $" MinW:{workerThreadsMin} MinC:{completionPortThreadsMin} ({Begin.TimeOfDay} - {Complete.TimeOfDay})";
+        }
+
+        [HttpGet("SetThreadPool/{value1}/{value2}")]
+        public string SetThreadPool(int value1, int value2)
+        {
+            ThreadPool.SetMinThreads(value1, value2);
+
+            string result = "OK";
+            int workerThreadsAvailable;
+            int completionPortThreadsAvailable;
+            int workerThreadsMax;
+            int completionPortThreadsMax;
+            int workerThreadsMin;
+            int completionPortThreadsMin;
+            ThreadPool.GetAvailableThreads(out workerThreadsAvailable, out completionPortThreadsAvailable);
+            ThreadPool.GetMaxThreads(out workerThreadsMax, out completionPortThreadsMax);
+            ThreadPool.GetMinThreads(out workerThreadsMin, out completionPortThreadsMin);
+
+            DateTime Complete = DateTime.Now;
+            result = "OK " + $"AW:{workerThreadsAvailable} AC:{completionPortThreadsAvailable}" +
+                $" MaxW:{workerThreadsMax} MaxC:{completionPortThreadsMax}" +
+                $" MinW:{workerThreadsMin} MinC:{completionPortThreadsMin} ";
+
+            return result;
+        }
+
+        [HttpGet("SetMaxThreadPool/{value1}/{value2}")]
+        public string SetMaxThreadPool(int value1, int value2)
+        {
+            ThreadPool.SetMaxThreads(value1, value2);
+
+            string result = "OK";
+            int workerThreadsAvailable;
+            int completionPortThreadsAvailable;
+            int workerThreadsMax;
+            int completionPortThreadsMax;
+            int workerThreadsMin;
+            int completionPortThreadsMin;
+            ThreadPool.GetAvailableThreads(out workerThreadsAvailable, out completionPortThreadsAvailable);
+            ThreadPool.GetMaxThreads(out workerThreadsMax, out completionPortThreadsMax);
+            ThreadPool.GetMinThreads(out workerThreadsMin, out completionPortThreadsMin);
+
+            DateTime Complete = DateTime.Now;
+            result = "OK " + $"AW:{workerThreadsAvailable} AC:{completionPortThreadsAvailable}" +
+                $" MaxW:{workerThreadsMax} MaxC:{completionPortThreadsMax}" +
+                $" MinW:{workerThreadsMin} MinC:{completionPortThreadsMin} ";
+
+            return result;
+        }
+
+
+        [HttpGet("GetThreadPool")]
+        public string GetThreadPool()
+        {
+            string result = "";
+            int workerThreadsAvailable;
+            int completionPortThreadsAvailable;
+            int workerThreadsMax;
+            int completionPortThreadsMax;
+            int workerThreadsMin;
+            int completionPortThreadsMin;
+            ThreadPool.GetAvailableThreads(out workerThreadsAvailable, out completionPortThreadsAvailable);
+            ThreadPool.GetMaxThreads(out workerThreadsMax, out completionPortThreadsMax);
+            ThreadPool.GetMinThreads(out workerThreadsMin, out completionPortThreadsMin);
+
+            DateTime Complete = DateTime.Now;
+            result = " " + $"AW:{workerThreadsAvailable} AC:{completionPortThreadsAvailable}" +
+                $" MaxW:{workerThreadsMax} MaxC:{completionPortThreadsMax}" +
+                $" MinW:{workerThreadsMin} MinC:{completionPortThreadsMin} ";
+
+            return result;
+        }
+    }
+}
